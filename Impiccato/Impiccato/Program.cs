91cbb0ff88b9c778c31853d804b6d24555623ff2
caibugattiruben <caibugatti.ruben@franchettisalviani.net>
2025-03-14 22:01:26 +0100

    creo matrice
    non va ancora

---
+++ Impiccato/Impiccato/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+void creoMatrice(string[,] NomiCompleti)
+{
+    string[] nomi1;
+    string path = "nomi.txt";
+    StreamReader r = new StreamReader(path);
+    string nomi = r.ReadLine();
+    int cont = 0;
+    while (nomi != "")
+    {
+        nomi1 = nomi.Split(',');
+        for (int i = 0; i < NomiCompleti.GetLength(1); i++)
+        {
+            NomiCompleti[cont,i]=nomi1[i];
+        }
+        cont++;
+        nomi = r.ReadLine();
+    }
+}
+string[,] nomi=new string[5,51];
+creoMatrice(nomi);
+for (int i = 0; i < nomi.GetLength(0); i++)
+{
+    for (int j = 0; j < nomi.GetLength(1); j++)
+    {
+        Console.Write(nomi[i,j]);
+    }
+    Console.WriteLine();
+}
+
+
