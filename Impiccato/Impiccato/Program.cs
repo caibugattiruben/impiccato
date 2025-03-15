9af29ab361f3e6436646a8a9353e10191858a233
caibugattiruben <caibugatti.ruben@franchettisalviani.net>
2025-03-15 09:08:26 +0100

    ok
    funzia

--- Impiccato/Impiccato/Program.cs
+++ Impiccato/Impiccato/Program.cs
@@ -1,5 +1,4 @@
 ﻿
-
 void creoMatrice(string[,] NomiCompleti)
 {
     string[] nomi1;
@@ -7,26 +6,54 @@ void creoMatrice(string[,] NomiCompleti)
     StreamReader r = new StreamReader(path);
     string nomi = r.ReadLine();
     int cont = 0;
-    while (nomi != "")
+    while (nomi != null)
     {
         nomi1 = nomi.Split(',');
-        for (int i = 0; i < NomiCompleti.GetLength(1); i++)
+        for (int i = 0; i < NomiCompleti.GetLength(1)-1; i++)
         {
-            NomiCompleti[cont,i]=nomi1[i];
+            NomiCompleti[cont, i] = nomi1[i];
         }
         cont++;
+        nomi = "";
         nomi = r.ReadLine();
     }
 }
+string scelgoParola(string[,] m)
+{
+    string parola;
+    Random rnd=new Random();   
+    Console.WriteLine("Vuoi scegliere il tema della tua parola? (s o n) (se sbagli a digitare il computer sceglierà per te)");
+    string risposta = Console.ReadLine();
+    risposta=risposta.Trim().ToLower();
+    int riga = 0,colonna=0;
+    if (risposta =="s")
+    {
+        Console.WriteLine("dimmi il tema della tua parola (calcio, verbi, frutta e verdura, animali oppure nazioni) (se sbagli verrà scelto calcio)");
+        string Tema=Console.ReadLine();
+        Tema = Tema.Trim().ToLower();
+        for(int i = 0; i < m.GetLength(0); i++)
+        {
+            if (Tema == m[i, 0])
+            {
+                riga = i;
+            }
+        }
+
+                
+    }
+    else
+    {
+        riga=rnd.Next(0,5);
+    }
+    colonna=rnd.Next(1,52);
+    parola = m[riga, colonna];
+    return parola;
+}
+
 string[,] nomi=new string[5,51];
 creoMatrice(nomi);
-for (int i = 0; i < nomi.GetLength(0); i++)
-{
-    for (int j = 0; j < nomi.GetLength(1); j++)
-    {
-        Console.Write(nomi[i,j]);
-    }
-    Console.WriteLine();
-}
+string parola=scelgoParola(nomi);
+Console.WriteLine(parola);
+
 
 
